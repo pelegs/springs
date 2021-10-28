5e2902241a1363d5199cdc8d821c39fb5e2516c5
Peleg Sapir <pelegs@gmail.com>
2021-10-28 21:32:49 +0200

    Hooray

--- libspring.py
+++ libspring.py
@@ -9,7 +9,7 @@ def normalize(v):
     if L != 0:
         return v/L
     else:
-        return 0
+        return np.zeros(2)
 
 def scale(v, n):
     return n * normalize(v)
@@ -19,7 +19,7 @@ def look_at(v1, v2):
 
 
 class Spring:
-    def __init__(def, obj1, obj2, L0=1, k=1):
+    def __init__(self, obj1, obj2, L0=1, k=1):
         self.obj1 = obj1
         self.obj2 = obj2
         self.L0 = L0
@@ -31,18 +31,20 @@ class Spring:
         L = abs(self.obj1.pos - self.obj2.pos)
         dx = L - self.L0
         F = -self.k * dx
-        self.F1 = F * normalize(look_at(self.obj1.pos, self.obj2.pos))
-        self.F2 = -self.F1
+        F1 = scale(look_at(self.obj1.pos, self.obj2.pos), F)
+        F2 = -self.F1
+        return F1, F2
 
 
 class Object:
-    def __init__(self, pos=np.zeros(2), vel=np.zeros(2), mass=1):
+    def __init__(self, pos=np.zeros(2), vel=np.zeros(2), mass=1, mu=1):
         self.pos = pos
         self.vel = vel
         self.mass = mass
         self.m_ = 1./mass
         self.reset_forces()
         self.F = np.zeros(2)
+        self.mu = mu
 
     def reset_forces(self):
         self.forces = np.zeros(2)
@@ -54,8 +56,12 @@ class Object:
         return np.sum(self.forces, axis=0)
 
     def move(self, dt=0.01):
+        Fd = -scale(self.vel, self.vel**2) * self.mu
+        self.add_force(Fd)
         F = self.sum_forces()
         a = F*self.m_
         # Not the best integrator...
         self.vel = self.vel + a*dt
         self.pos = self.pos + self.vel*dt
+        # Reset forces
+        self.reset_forces()
---
+++ test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: iso-8859-15 -*-
+
+import numpy as np
+from libspring import *
+import sys
+import pygame
+from pygame.locals import *
+#from pygame.color import THECOLORS as COLORS
+#from os import system
+#clear = lambda: system('clear')
+
+
+width, height = 700, 700
+pygame.init()
+fps = 60
+fpsClock = pygame.time.Clock()
+screen = pygame.display.set_mode((width, height))
+
+# Objects
+mouse_obj = Object()
+mass_obj = Object(pos=np.array([width/2, height/2]), mass=1, mu=0.05)
+spring = Spring(obj1=mouse_obj, obj2=mass_obj, L0=50, k=100)
+
+# Mouse pos
+pygame.mouse.set_pos([width//2, height//2-50])
+
+# Game loop
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            run = False
+            break
+
+    # Mechanism
+    x, y = pygame.mouse.get_pos()
+    mouse_obj.pos = np.array([x,y])
+    F1, F2 = spring.forces()
+    mass_obj.add_force(F1)
+    mass_obj.add_force(np.array([0,10000]))
+    mass_obj.move(dt=0.01)
+
+    # Reset screen
+    screen.fill([0,0,0])
+
+    # Draw
+    pygame.draw.circle(screen, [0,100,255], mouse_obj.pos.astype(int), 10)
+    pygame.draw.circle(screen, [255,0,0], mass_obj.pos.astype(int), 10)
+    pygame.draw.line(
+        screen, [255,255,255],
+        spring.obj1.pos.astype(int),
+        spring.obj2.pos.astype(int),
+        width=3
+    )
+
+    # Update
+    pygame.display.flip()
+    fpsClock.tick(fps)
+
+pygame.quit()
